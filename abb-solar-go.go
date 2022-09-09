742ffc6aed5e382f80cc254c26d7a696996a7e84
njoseph <njoseph@bravurasolutions.com>
2022-09-09 18:10:37 +1000

    initial commit

---
+++ abb-solar-go.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/antchfx/jsonquery"
+)
+
+// curl http://user21:password@192.168.80.172/v1/livedata
+func main() {
+
+	http.HandleFunc("/metrics", getMetrics())
+	log.Fatal(http.ListenAndServe(fmt.Sprint(":8080"), nil))
+}
+func getMetrics() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			userName := os.Getenv("abb_user")
+			password := os.Getenv("abb_password")
+			url := os.Getenv("abb_ip")
+			endpoint := strings.ReplaceAll(fmt.Sprintf("http://%s:%s@ %s/v1/livedata", userName, password, url), " ", "")
+			doc, err := jsonquery.LoadURL(endpoint)
+			if err != nil {
+				panic(err)
+			}
+
+			list := jsonquery.Find(doc, "//points/*")
+			for _, n := range list {
+
+				myMap := n.Value().(map[string]interface{})
+				key := fmt.Sprint(myMap["name"])
+				val := fmt.Sprint(myMap["value"])
+				w.Write([]byte(key))
+				w.Write([]byte(val))
+				//YAHOOO
+				//fmt.Println(n.Value().name)
+			}
+		}
+
+	})
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module nj/abb-solar-go
+
+go 1.19
+
+require (
+	github.com/antchfx/jsonquery v1.3.0 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+)
